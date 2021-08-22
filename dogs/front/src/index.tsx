369a38ac2e23bfe59f425c82fed45be7fabb0c0e
nakamura <nakamura0067@gmail.com>
2021-08-23 00:58:57 +0900

    画面実装

--- dogs/front/src/index.tsx
+++ dogs/front/src/index.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom';
+
+import Login from './Login';
+import Dogs from './Dogs';
+import Navigation from './Navigation';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/"
+          render={() => {
+            return (
+                <Redirect to="/login" />
+              )
+          }}
+        />
+        <Route exact path="/login" component={Login} /> 
+        <Route path="/">
+          <Route path="/dogs" component={Dogs}/>
+          <Navigation/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
