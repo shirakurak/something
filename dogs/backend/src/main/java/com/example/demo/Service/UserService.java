07a4765a3a55009017b4f1ef815e6e2e2ccf05a5
Kenichi SHIRAKURA <surakura@hotmail.co.jp>
2021-09-12 14:23:03 +0900

    Serviceを作成

---
+++ dogs/backend/src/main/java/com/example/demo/Service/UserService.java
@@ -0,0 +1,18 @@
+package com.example.demo.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.User;
+import com.example.demo.repositoty.UserMapper;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserMapper mapper;
+	
+	public User selectUser(String name, String password) {
+		return mapper.selectUser(name, password);
+	}
+}
