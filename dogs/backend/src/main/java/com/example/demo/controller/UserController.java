713ed75e189fa8aaec1492db68472c392881a431
Kenichi SHIRAKURA <surakura@hotmail.co.jp>
2021-09-20 10:12:16 +0900

    疎通のために実装を修正（疎通自体の確認はできていない）

--- dogs/backend/src/main/java/com/example/demo/controller/UserController.java
+++ dogs/backend/src/main/java/com/example/demo/controller/UserController.java
@@ -1,25 +1,38 @@
 package com.example.demo.controller;
 
-import java.util.Objects;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.service.UserService;
 
 /**
  * とりあえず、MySQLからデータを取ってこれるか検証する用に作成したController
  */
-@Controller
+//@Controller
+//@RestController
+//@RequestMapping("/hello")
+@RestController
 public class UserController {
 	
 	@Autowired
 	UserService service;
 	
-	public String selectUser() {
-		if(Objects.isNull(service.selectUser("taro", "password"))) {
-			return "ない";
-		}
-		return "あった";
+//	@RequestMapping(method=RequestMethod.GET)
+//	@GetMapping
+//	public String hello() {
+//		
+//		System.out.println("hello");
+//		
+//		if(Objects.isNull(service.selectUser("taro", "password"))) {
+//			return "ない";
+//		}
+//		return "あった";
+//	}
+	
+	// 疎通できていない
+	@GetMapping("/test1")
+	public String write1() {
+		return "Hello World1";
 	}
 }
