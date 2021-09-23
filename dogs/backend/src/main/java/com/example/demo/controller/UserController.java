620c97412baa02f2c7ee1b348f248ef27224237f
Kenichi SHIRAKURA <surakura@hotmail.co.jp>
2021-09-23 11:56:04 +0900

    serviceを呼び出す疎通まで確認した実装に修正

--- dogs/backend/src/main/java/com/example/demo/controller/UserController.java
+++ dogs/backend/src/main/java/com/example/demo/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import java.util.Objects;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,30 +11,20 @@ import com.example.demo.service.UserService;
 /**
  * とりあえず、MySQLからデータを取ってこれるか検証する用に作成したController
  */
-//@Controller
-//@RestController
-//@RequestMapping("/hello")
 @RestController
 public class UserController {
 	
 	@Autowired
 	UserService service;
 	
-//	@RequestMapping(method=RequestMethod.GET)
-//	@GetMapping
-//	public String hello() {
-//		
-//		System.out.println("hello");
-//		
-//		if(Objects.isNull(service.selectUser("taro", "password"))) {
-//			return "ない";
-//		}
-//		return "あった";
-//	}
-	
-	// 疎通できていない
 	@GetMapping("/test1")
-	public String write1() {
-		return "Hello World1";
+	public String hello() {
+		
+		System.out.println("hello()メソッドが呼び出されました。");
+		
+		if(Objects.isNull(service.selectUser("taro", "password"))) {
+			return "ない";
+		}
+		return "あった";
 	}
 }
