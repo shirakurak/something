c1742dfe5ec1093f6cc722448689adb1499b2fdd
Kenichi SHIRAKURA <surakura@hotmail.co.jp>
2021-09-23 13:14:32 +0900

    dogに関連するクラスとxmlファイルを追加

---
+++ dogs/backend/src/main/java/com/example/demo/entity/Dog.java
@@ -0,0 +1,10 @@
+package com.example.demo.entity;
+
+import lombok.Data;
+
+@Data
+public class Dog {
+	private int dogId;
+	private String dogName;
+	private String area;
+}
---
+++ dogs/backend/src/main/java/com/example/demo/service/DogService.java
@@ -0,0 +1,20 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Dog;
+import com.example.demo.repository.DogMapper;
+
+@Service
+public class DogService {
+	
+	@Autowired
+	private DogMapper mapper;
+	
+	public List<Dog> selectDogs() {
+		return mapper.selectDogs();
+	}
+}
