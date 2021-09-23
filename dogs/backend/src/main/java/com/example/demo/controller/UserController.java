a036162a67ec34f75758cda127d434e6ceb7eb30
Kenichi SHIRAKURA <surakura@hotmail.co.jp>
2021-09-23 13:14:58 +0900

    Controllerに検証用メソッドを追加

--- dogs/backend/src/main/java/com/example/demo/controller/UserController.java
+++ dogs/backend/src/main/java/com/example/demo/controller/UserController.java
@@ -1,30 +1,68 @@
 package com.example.demo.controller;
 
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.entity.Dog;
+import com.example.demo.service.DogService;
 import com.example.demo.service.UserService;
 
 /**
- * とりあえず、MySQLからデータを取ってこれるか検証する用に作成したController
+ * 作成中
  */
 @RestController
 public class UserController {
 	
 	@Autowired
-	UserService service;
+	UserService userService;
+	@Autowired
+	DogService dogService;
 	
+	/**
+	 * MySQLからデータを取ってこれるか検証用メソッド
+	 * @return あった or ない
+	 */
 	@GetMapping("/test1")
 	public String hello() {
 		
 		System.out.println("hello()メソッドが呼び出されました。");
 		
-		if(Objects.isNull(service.selectUser("taro", "password"))) {
+		if(Objects.isNull(userService.selectUser("taro", "password"))) {
 			return "ない";
 		}
 		return "あった";
+	}
+	
+	/**
+	 * ログイン検証用メソッド。
+	 * @return ログイン可否
+	 */
+	@GetMapping("/login")
+	public String login() {
+		
+		return "idとパスワードに対応するユーザがいたら、OKとか返す";
+		
+//		if(Objects.isNull(userService.selectUser("taro", "password"))) {
+//			return "ない";
+//		}
+//		return "あった";
+	}
+	
+	/**
+	 * ワンちゃん情報の取得用メソッド。
+	 * @return ワンちゃんたち
+	 */
+	@GetMapping("/dogs")
+	public String getDogs() {
+		
+		List<Dog> dogs = dogService.selectDogs();
+		
+		System.out.println(dogs);
+		
+		return "dogsたちをresponseとして返却する。";
 	}
 }
