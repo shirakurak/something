b31de0687db8ed0ea54c0d340666c88836a6011c
Kenichi SHIRAKURA <surakura@hotmail.co.jp>
2021-11-30 22:11:14 +0900

    #16のコメントの指摘を反映：カラム名のprefixとidの文字列化対応

--- dogs/docs/20_db/test_data.sql
+++ dogs/docs/20_db/test_data.sql
@@ -1,4 +1,4 @@
-insert into users values (1, 'taro', 'password');
+insert into users values ('0000000001', 'taro', 'password');
 
-insert into dogs values (3001, 'ケルベロス', '群馬'); 
-insert into dogs values (3002, 'チワワ', '大阪');
+insert into dogs values ('3000000001', 'ケルベロス', '群馬'); 
+insert into dogs values ('3000000002', 'チワワ', '大阪');
