792210d2b518968caf7575639cdc5cfc4ae0e4ab
nakamura <nakamura0067@gmail.com>
2021-12-06 00:52:08 +0900

    ワンちゃん一覧取得処理の修正

--- dogs/front/src/Dogs.tsx
+++ dogs/front/src/Dogs.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
 import ImageList from '@material-ui/core/ImageList';
 import ImageListItem from '@material-ui/core/ImageListItem';
@@ -6,7 +6,7 @@ import ImageListItemBar from '@material-ui/core/ImageListItemBar';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import IconButton from '@material-ui/core/IconButton';
 import InfoIcon from '@material-ui/icons/Info';
-import itemData from './itemData';
+import axios from 'axios';
 
 import './Dogs.css';
 
@@ -31,8 +31,22 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
+const DOGS_URL = "http://localhost:1598/api/dogs"
+
 export default function Dogs() {
   const classes = useStyles();
+  const [dogs,setDogs] = useState([{id:"",name:"",area:"",img:""}]);
+  // 初回レンダリング時のみに実行
+  useEffect(() => {
+    axios.get(DOGS_URL)
+      .then(res => {
+        console.log('res:', res)
+        setDogs(res.data);
+      })
+      .catch(err => {
+        console.log('err:', err);
+      })
+  },[]);
 
   return (
     <div className={classes.root}>
@@ -40,17 +54,16 @@ export default function Dogs() {
         <ImageListItem key="Subheader" cols={4} style={{ height: 'auto' }}>
           <ListSubheader component="div">ワンちゃん一覧</ListSubheader>
         </ImageListItem>
-        {itemData.map((item) => (
-          <ImageListItem key={item.img} className="dog-images">
-            {/* FIXME:バックエンド通信 */}
-            <img src={item.img} alt={item.name} onClick={()=>{alert(`${item.name}の詳細画面を表示する`)}}/>
+        {dogs.map((dog) => (
+          <ImageListItem key={dog.img} className="dog-images">
+            <img src={dog.img} alt={dog.name} onClick={()=>{alert(`${dog.name}の詳細画面を表示する`)}}/>
             <ImageListItemBar
-              title={item.name}
-              subtitle={<span>{item.area}</span>}
+              title={dog.name}
+              subtitle={<span>{dog.area}</span>}
               style={{height:'auto'}}
               actionIcon={
-                <IconButton aria-label={`info about ${item.name}`} className={classes.icon}>
-                  <InfoIcon onClick={()=>{alert(`${item.name}の情報を表示する`)}} />
+                <IconButton aria-label={`info about ${dog.name}`} className={classes.icon}>
+                  <InfoIcon onClick={()=>{alert(`${dog.name}の情報を表示する`)}} />
                 </IconButton>
               }
             />
