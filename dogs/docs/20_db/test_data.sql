d9759d756c70ceb6c0af874f852f964a886dbbe0
Kenichi SHIRAKURA <surakura@hotmail.co.jp>
2021-12-31 11:39:22 +0900

    test_data.sqlの修正

--- dogs/docs/20_db/test_data.sql
+++ dogs/docs/20_db/test_data.sql
@@ -2,8 +2,14 @@ delete from users;
 delete from dogs;
 
 -- ユーザ（ワンちゃん志願者）
-insert into users values ('0000000001', 'taro', 'password');
+insert into users values ('shirakurak', '白倉K', 'P@ssw0rd');
+insert into users values ('nakamura67', '中村S', 'password');
+insert into users values ('taro_12$&%', '山田太郎', 'password');
 
 -- ワンちゃん
-insert into dogs values ('3000000001', 'ケルベロス', '群馬', 'dog1.jpg'); 
-insert into dogs values ('3000000002', 'チワワ', '大阪', 'dog2.jpg');
+insert into dogs values ('3000000001', 'ケルベロス', '群馬', 'https://github.com/shirakurak/something/blob/17-image-coding/dogs/backend/src/main/resources/images/dog1.jpg?raw=true'); 
+insert into dogs values ('3000000002', 'チワワ', '大阪', 'https://github.com/shirakurak/something/blob/17-image-coding/dogs/backend/src/main/resources/images/dog2.jpg?raw=true');
+insert into dogs values ('3000000003', 'ハチ公', '渋谷区道玄坂２丁目１', 'https://github.com/shirakurak/something/blob/17-image-coding/dogs/backend/src/main/resources/images/dog3.jpg?raw=true');
+insert into dogs values ('3000000004', 'パトラッシュ', 'ベルギー', 'https://github.com/shirakurak/something/blob/17-image-coding/dogs/backend/src/main/resources/images/dog4.jpg?raw=true');
+insert into dogs values ('3000000005', 'イッヌ', '北海道', 'https://github.com/shirakurak/something/blob/17-image-coding/dogs/backend/src/main/resources/images/dog5.jpg?raw=true');
+insert into dogs values ('3000000006', '犬じゃなくね？こいつ', 'YOKOHAMA', 'https://github.com/shirakurak/something/blob/17-image-coding/dogs/backend/src/main/resources/images/dog6.jpg?raw=true');
