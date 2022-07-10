dd590d52e8352a6f237037a091ee578e719e8647
nakamura <nakamura0067@gmail.com>
2022-07-10 13:28:04 +0900

    react-frontとangular-frontに分割

---
+++ dogs/react-front/src/Login.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+}));
+
+export default function Login() {
+  const classes = useStyles();
+  const history = useHistory();
+
+  const handleClick = (userId:any,password:any) => {
+    if (userId !== null && password !== null){
+      axios.get(`http://localhost:1598/api/login/userId/` + userId.value + `/password/` + password.value)
+        .then(res => {
+          console.log('res:', res)
+          if(res.data === 1){
+            history.push('/dogs')
+          } else {
+            alert("ログイン失敗")
+          }
+        })
+        .catch(err => {
+          console.log('err:', err);
+        })
+    }
+  }
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+         <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="userId"
+          label="User ID"
+          name="userId"
+          autoFocus
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+        />
+        <Button
+          fullWidth
+          variant="contained"
+          color="primary"
+          onClick={() => handleClick(document.getElementById("userId"),document.getElementById("password"))}
+       >
+          Sign In
+        </Button>
+      </div>
+    </Container>
+  );
+}
